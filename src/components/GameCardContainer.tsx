61c11259dab10d3b303119494662690e14589e92
Mansor Azimi <mansorazimi17@gmail.com>
2024-06-07 23:27:18 +0430

    display genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="300px" borderRadius={"10px"} overflow={"hidden"}>
+    <Box borderRadius={"10px"} overflow={"hidden"}>
       {children}
     </Box>
   );
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,29 @@
+import { List, ListItem, HStack, Image, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/getCroppedImageUrl";
 
 const GenreList = () => {
   //   const { data } = useData<Genre>("/genres");
   const { data } = useGenres();
   return (
     <>
-      <ul>
+      <List>
         {data.map((gen) => (
-          <li key={gen.id}>{gen.name}</li>
+          <ListItem padding="5px" key={gen.id}>
+            <HStack key={gen.id}>
+              <Image
+                key={gen.id}
+                boxSize="38px"
+                borderRadius={8}
+                src={getCroppedImageUrl(gen.image_background)}
+              />
+              <Text key={gen.id} fontSize="lg">
+                {gen.name}
+              </Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
